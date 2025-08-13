cea494a36bc08a4506ceb7849c4b63254db7c8c2
Mark Newcomb <markenewcomb@gmail.com>
2025-08-13 18:20:42 -0500

    Commit

--- src/main/python/main_dev.py
+++ src/main/python/main_dev.py
@@ -1,3 +1,42 @@
 # Main Python Development Script
+import random
+import sys
 
 print(f"Hello, world!")
+
+if len(sys.argv) > 1:
+    for i in sys.argv:
+        print(i)
+
+# Data types
+city = "Whittier"
+birth_year = 1987
+starting_wage = 6.75
+car_years = [1967, 2007, 2012, 2018, 2022, 2024]
+is_married = True
+next_city = None
+family_members = {"Dad": "Mark", "Mom": "Marissa", "Daughter": "Hazel", "Son": "Hudson"}
+
+# my_favorite_num = input("What is your favorite number?")
+my_favorite_num = 7
+print(f"My fave number plus 5 is {int(my_favorite_num) + 5}")
+
+for i in [city, birth_year, starting_wage, car_years, is_married, next_city]:
+    print(f"Data type for {i} is {type(i)}")
+
+for key in family_members.keys():
+    print(f"{key}")
+
+for key,value in family_members.items():
+    print(f"{value} is the {key} in the family.")
+
+random_num = random.randrange(0,100)
+print(f"Random number is {random_num}")
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
+
+# Continue at W3 with Lists, Tuples, Sets, etc. 
--- src/main/scripts/scripts.sh
+++ src/main/scripts/scripts.sh
@@ -7,7 +7,7 @@ bash --version
 pwd
 
 mkdir -p practice_bash/test_bash
-cd practice_bash
+cd practice_bash || ( echo "Error trying to cd...exiting."; exit )
 touch test_file_1.txt test_file_2.txt test_file_3.txt
 ls -la
 
@@ -22,3 +22,43 @@ rm -f test_file_5.txt
 # alias ll='ls -la'
 
 # Continue at W3 Schools: Text Processing section
+my_name="Mark"
+echo "Hello, ${my_name}"
+
+my_name_function(){
+  name=$1
+  echo "${name} was given to this function"
+}
+
+my_name_function 'Tim'
+my_name_function 'Mike'
+
+my_number=$(( 2 + 4 ))
+echo $my_number
+my_command_result=$(cat test_file_1.txt)
+echo "${my_command_result}"
+
+if [[ $my_name == 'Mark' ]]; then
+  echo 'Hi Mark'
+fi
+if (( 6 == 6 )); then
+  echo 'Number is 6'
+fi
+
+family_members=('Mark' 'Marissa' 'Hazel' 'Hudson')
+for member in "${family_members[@]}"; do
+  echo "$member"
+done
+
+if [[ $my_name == 'Mark' ]] && (( 2 == 3 )); then
+  echo 'My name is Mark and the number is 2'
+elif [[ $my_name == 'Mar' ]]; then
+  echo 'Name is Mar'
+else
+  echo 'Who are you?'
+fi
+
+for i in {1..5}; do
+  echo $i
+done
+
